90fde7821f5b1ed5de880e1908329c964fc0ce47
Timpo88m <Timpo88m@gmail.com>
2017-11-02 22:25:06 -0500

    Adding VS2017 compiled SFML directly to repo. Updating project.

--- Fallen_Forever/GameObject.h
+++ Fallen_Forever/GameObject.h
@@ -13,6 +13,21 @@ public:
 	void ControllerTick(sf::Time _DeltaTime) {}
 	void SetController(Controller* _Controller) {}
 	Controller* GetController() { return mController; }
+
+protected:
+	std::pair<double, double> mPosition;	// The object's position
+	std::pair<double, double> mSize;		// The object's size
+	std::pair<double, double> mVelocity;	// The object's velocity
+
+	double mRotation;						// The Object's rotation
+	double mMass;							// The Object's mass
+	double mIntegrity;						// The Object's Hitpoints
+	double mHardness;						// The Object's Resistance
+	
+	bool mPhysical;							// Whether or not the Object can collide.
+
+	int  mLayer;							// The Object's layer (for rendering and physics)
+
 private:
 
 	Controller* mController;
